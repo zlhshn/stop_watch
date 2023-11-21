e43f6904644113bb18b92610f652eed6862edc42
zlhshn <zsahin4401@gmail.com>
2023-11-22 01:51:55 +0300

    clock

--- stopwatch/main.js
+++ stopwatch/main.js
@@ -1,22 +1,25 @@
 import './style.scss';
 
-let [micro, seconds, minutes, hours] = [0, 0, 0, 0];
+let [salise, seconds, minutes, hours] = [0, 0, 0, 0];
+
+
 let displayTime = document.getElementById('watch');
 let startButton = document.getElementById('start');
 let resetButton = document.getElementById('reset'); // Added resetButton
 let tourList = document.getElementById('tour');
-let timer;
+let timer = 0
 
 const showTime = () => {
-    micro++;
-    if (micro == 100) {
-        micro = 0;
+    salise++;
+    if (salise == 100) {
+        salise = 0;
         seconds++;
     }
 
     if (seconds == 60) {
         seconds = 0;
         minutes++;
+
         if (minutes == 60) {
             minutes = 0;
             hours++;
@@ -26,17 +29,37 @@ const showTime = () => {
     let h = hours < 10 ? '0' + hours : hours;
     let m = minutes < 10 ? '0' + minutes : minutes;
     let s = seconds < 10 ? '0' + seconds : seconds;
-    let mc = micro < 10 ? '0' + micro : micro;
+    let sl = salise < 10 ? '0' + salise : salise;
 
-    displayTime.innerHTML = h + ':' + m + ':' + s + ':' + mc;
+    displayTime.innerHTML = h + ':' + m + ':' + s+ ':' + sl;
 };
 
+
+
+
+
 const watchStart = () => {
-    if (timer !== null) {
+    if (timer !== 0) {
         clearInterval(timer);
     }
     timer = setInterval(showTime, 10);
 };
+
+
+
+
+// startButton.addEventListener('click',()=>{
+
+//     watchStart()
+// })
+
+
+// resetButton.addEventListener('click',()=>{
+//     clearInterval(timer);
+//     displayTime.innerHTML  = '00:00:00:00'
+
+// })
+
 
 startButton.addEventListener('click', (e) => {
     if (e.target.classList.contains('dur')) {
@@ -58,7 +81,7 @@ let tour = 1; // Initialize tour outside the event listener
 resetButton.addEventListener('click', (e) => {
     if (e.target.innerHTML === 'Reset') {
         clearInterval(timer);
-        [micro, seconds, minutes, hours] = [0, 0, 0, 0];
+        [salise, seconds, minutes, hours] = [0, 0, 0, 0];
         displayTime.innerHTML = '00:00:00:00';
         tourList.innerHTML = '';
         tour = 1; // Reset tour count
@@ -73,3 +96,28 @@ resetButton.addEventListener('click', (e) => {
 });
 
 
+
+
+const numberHours = document.querySelector('.number-hours')
+const clockbarseconds = document.querySelector('.clock-bar-seconds')
+
+const numbersElement = []
+
+for (let i = 5; i <= 60; i += 5){
+
+    numbersElement.push(`<span style="--index:${i}*5"><p>${i}</p></span>`)
+}
+
+
+    numberHours.insertAdjacentHTML ("afterbegin", numbersElement.join(""));
+
+
+ const clockbarsecondsElement = []
+for (let i = 1; i <= 60; i++){
+
+    clockbarsecondsElement.push(`<span style="--index:${i}"><p>|</p></span>`)
+}
+
+
+clockbarseconds.insertAdjacentHTML ("afterbegin", clockbarsecondsElement.join(""));
+    
